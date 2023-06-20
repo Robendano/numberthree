69593d74e331509dc10f660b6b6e1760528c3b70
Robendano <78977590+Robendano@users.noreply.github.com>
2023-06-20 15:54:34 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const handleInputChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  const handleAddTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <div className="task-input">
+        <input
+          type="text"
+          placeholder="Enter a new task"
+          value={newTask}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleAddTask}>Add Task</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
